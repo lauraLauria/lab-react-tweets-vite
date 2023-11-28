357e656de21d81b789654e9baa148e89ed250516
Laura Lauria <lauralauria@Air-von-Laura.local>
2023-11-28 09:26:35 +0100

    done

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,26 +1,27 @@
-function Tweet() {
+import ProfileImage from "./ProfileImage"; 
+
+function Tweet({
+  tweet: {
+    user: { name, image, handle },
+    timestamp,
+    message,
+  },
+}) {
   return (
     <div className="tweet">
-      <img
-        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"
-        className="profile"
-        alt="profile"
-      />
+       <ProfileImage image={image} alt="profile" />
 
       <div className="body">
         <div className="top">
           <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
+            <span className="name">{name}</span>
+            <span className="handle">{handle}</span>
           </span>
 
-          <span className="timestamp">Nov 30, 2020</span>
+          <span className="timestamp">{timestamp}</span>
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
+        <p className="message">{message}</p>
 
         <div className="actions">
           {/* Font Awesome icons */}
